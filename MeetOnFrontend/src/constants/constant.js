d6182d31938589f8ee1de51b091d9b1bdddd653c
Zorgont <zorgont@gmail.com>
2022-04-27 12:27:26 +0300

    Add dockerfiles and build scripts

--- MeetOnFrontend/src/constants/constant.js
+++ MeetOnFrontend/src/constants/constant.js
@@ -2,11 +2,12 @@ export const API_GATEWAY_URL = "http://localhost:8989";
 export const API_BASE_URL = API_GATEWAY_URL;
 export const WS_BASE_URL = API_GATEWAY_URL;
 export const AM_BASE_URL = API_GATEWAY_URL;
+export const AM_OAUTH2_BASE_URL = "http://localhost:8989";
 
 export const ACCESS_TOKEN = "accessToken";
 
 export const OAUTH2_REDIRECT_URI = "http://localhost:3000/oauth2/redirect";
 
 export const GOOGLE = "google";
-export const GOOGLE_AUTH_URL = AM_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
-export const GITHUB_AUTH_URL = AM_BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
+export const GOOGLE_AUTH_URL = AM_OAUTH2_BASE_URL + "/oauth2/authorize/google?redirect_uri=" + OAUTH2_REDIRECT_URI;
+export const GITHUB_AUTH_URL = AM_OAUTH2_BASE_URL + "/oauth2/authorize/github?redirect_uri=" + OAUTH2_REDIRECT_URI;
--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@ cd ./authentication-manager
 source build.sh
 cd ../MeetOnBackend
 source build.sh
+cd ../api-gateway
+source build.sh
+cd ../service-registry
+source build.sh
 cd ../MeetOnFrontend
 source build.sh
 
