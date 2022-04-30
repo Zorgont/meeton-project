2ac778b3004fb6beb9c319a9145dd18251eac6c5
Zorgont <zorgont@gmail.com>
2022-04-30 15:34:32 +0300

    Enable OAuth2 support in docker

--- MeetOnFrontend/src/constants/constant.js
+++ MeetOnFrontend/src/constants/constant.js
@@ -2,7 +2,7 @@ export const API_GATEWAY_URL = "http://localhost:8989";
 export const API_BASE_URL = API_GATEWAY_URL;
 export const WS_BASE_URL = API_GATEWAY_URL;
 export const AM_BASE_URL = API_GATEWAY_URL;
-export const AM_OAUTH2_BASE_URL = "http://localhost:8989";
+export const AM_OAUTH2_BASE_URL = API_GATEWAY_URL;
 
 export const ACCESS_TOKEN = "accessToken";
 
